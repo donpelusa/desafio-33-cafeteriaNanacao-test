a1af6a78599decd4a7b963632a3370fc6b03b4fb
Alvaro Cortés Opazo <92953141+donpelusa@users.noreply.github.com>
2025-01-13 22:27:37 -0300

    Update server.spec.js

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,33 +1,33 @@
-const request = require("supertest");
-const server = require("../index");
+const request = require('supertest');
+const app = require('../index');
 
-const token = "cualquier-token-para-eltest";
-
-describe("Operaciones CRUD de cafes", () => {
-  it("en ruta /cafes deberia devolver un 200 ok y un array con 1 elemento como minimo", async () => {
-    const response = await request(server).get("/cafes");
-    const cafes = response.body;
+describe('Pruebas de la API de Cafetería Nanacao', () => {
+  test('GET /cafes Arreglo con al menos un objeto, devuelve 200', async () => {
+    const response = await request(app).get('/cafes');
     expect(response.status).toBe(200);
-    expect(cafes).toBeInstanceOf(Array);
-    expect(cafes.length).toBeGreaterThan(0);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.body.some((elemento) => typeof elemento === "object" && elemento !== null)).toBe(true);
+    expect(response.body.every((elemento) => typeof elemento === "object" && elemento !== null)).toBe(true);
   });
 
-  it("deberia devolver 404 al eliminar cafe sin id", async () => {
-    const notId = 398472934;
-    const response = await request(server)
-      .delete(`/cafes/:${notId}`)
-      .set("Authorization", `Bearer ${token}`);
+  test('DELETE /cafes/:id ID no existe, devuelve 404', async () => {
+    const response = await request(app)
+      .delete('/cafes/90') 
+      .set('Authorization', 'Bearer token'); 
     expect(response.status).toBe(404);
   });
 
-  it("deberia devolver 201 al hacer post a /cafes", async () => {
-    const response = await request(server).post("/cafes");
+  test('POST /cafes Agrega nuevo café, devuelve 201', async () => {
+    const newCafe = { id: 9999, nombre: 'Café nuevo de prueba' }; 
+    const response = await request(app).post('/cafes').send(newCafe);
     expect(response.status).toBe(201);
+    expect(response.body).toContainEqual(newCafe);
   });
 
-  it("deberia retornar 400 si se hace un PUT a cafes:id con un id no existente", async () => {
-    const notId = 293847;
-    const response = await request(server).put(`/cafes/:${notId}`);
+  test('PUT /cafes/:id ID no coincide, devuelve 400', async () => {
+    const updatedCafe = { id: 5, nombre: 'Café actualizado' };
+    const response = await request(app).put('/cafes/4').send(updatedCafe); 
     expect(response.status).toBe(400);
   });
 });
